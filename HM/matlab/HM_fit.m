9d52d93454021f4a91f48ab8518c0848dffd185b
AntaresNebula <56564928+buer19970329@users.noreply.github.com>
2021-09-26 13:55:00 +0800

    Update HM_fit.m

--- HM/matlab/HM_fit.m
+++ HM/matlab/HM_fit.m
@@ -1,22 +1,20 @@
-function [Xfit, LL, BIC] = HM_fit(a, seq,ro)
+function [Xfit, LL, BIC] = HM_fit(seq,resp,ro)
 
 
-matrix = size(a);
+matrix = size(seq);
 nsubj =matrix(1);
 
 for ns = 1:nsubj
-    obFunc = @(x) HM_lik(a(ns,:),seq, ro, x(1), x(2), x(3));
+    obFunc = @(x) HM_lik(seq(ns,:),resp, ro, x(1), x(2), x(3));
     
     X0 = [0.5,4,0.5];  % x 的初始点    
     % plot the objective function at each iteration
     options = optimset('PlotFcns',@optimplotfval);    
     [Xfit(ns,:), NegLL(ns,:)] = fminsearch(obFunc, X0, options);
-
-
-
+    
 
     LL(ns,:) = -NegLL(ns,:);
-    BIC(ns,:) = length(X0) * log(length(a)) + 2*NegLL(ns,:);
+    BIC(ns,:) = length(X0) * log(length(seq)) + 2*NegLL(ns,:);
 end
 
 LL = LL
